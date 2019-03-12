eac94eb53d3f834162cf4ca85456c77830f5e05f
Curiosity10 <alexds3462@gmail.com>
2019-03-12 22:03:17 +0300

    Updates

---
+++ precache-manifest.8ba5cebf901287f784e5f419b677ab22.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b429d889341aa25a2548",
+    "url": "/TodoApp/static/js/runtime~main.b429d889.js"
+  },
+  {
+    "revision": "f2dbc94385aed5e606b7",
+    "url": "/TodoApp/static/js/main.f2dbc943.chunk.js"
+  },
+  {
+    "revision": "4b6e8cb94b4e5c79626e",
+    "url": "/TodoApp/static/js/2.4b6e8cb9.chunk.js"
+  },
+  {
+    "revision": "f2dbc94385aed5e606b7",
+    "url": "/TodoApp/static/css/main.1f70f9c1.chunk.css"
+  },
+  {
+    "revision": "b24564642c0bfe38df50197c08e7ea45",
+    "url": "/TodoApp/index.html"
+  }
+];
